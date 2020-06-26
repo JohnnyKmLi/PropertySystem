9af58f179604e187bdb06e5c17dffa76c77c830f
JohnnyKmLi <44840419+JohnnyKmLi@users.noreply.github.com>
2020-06-26 16:04:42 +0100

    Update property controller
    This commit fixes issues for synching the local database with the remote

--- PropertySystemApplication/Model/PropertyModel.php
+++ PropertySystemApplication/Model/PropertyModel.php
@@ -1,42 +1,55 @@
 <?php
-    class PropertyModel {
-        private $conn;
+class PropertyModel
+{
+    private $conn;
 
-        public function __construct($conn) {
-            $this->conn = $conn;
-        }
+    public function __construct($conn)
+    {
+        $this->conn = $conn;
+    }
 
-        public function read() {
-            $query = 'SELECT *
+    public function read()
+    {
+        $query = 'SELECT *
                       FROM properties
                       INNER JOIN property_types
                       ON properties.property_type_id = property_types.id
                       ORDER BY properties.updated_at DESC';
 
-            $statement = $this->conn->prepare($query);
-            $statement->execute();
+        $statement = $this->conn->prepare($query);
+        $statement->execute();
 
-            return $statement;
-        }
+        return $statement;
+    }
 
-        public function update($property) {
-
+    public function update($property)
+    {
+        if (array_key_exists('property_type', $property)) {
+            $query = "UPDATE property_types
+                      SET
+                        title = '{$property['property_type']['title']}',
+                        description = '{$property['property_type']['description']}',
+                        updated_at = '{$property['property_type']['updated_at']}'
+                      WHERE id = {$property['property_type']['id']}";
+        } else {
             $query = "UPDATE property_types
                       SET
                         title = '{$property['title']}',
                         description = '{$property['description']}',
                         updated_at = '{$property['updated_at']}'
                       WHERE id = {$property['id']}";
-            $statement = $this->conn->prepare($query);
-            $statement->execute();
+        }
 
-            $county = str_replace("'", "''", $property['county'] );
-            $country = str_replace("'", "''", $property['country'] );
-            $town = str_replace("'", "''", $property['town'] );
-            $description = str_replace("'", "''", $property['description'] );
-            $address = str_replace("'", "''", $property['address'] );
+        $statement = $this->conn->prepare($query);
+        $statement->execute();
 
-            $query = "UPDATE properties
+        $county = str_replace("'", "''", $property['county']);
+        $country = str_replace("'", "''", $property['country']);
+        $town = str_replace("'", "''", $property['town']);
+        $description = str_replace("'", "''", $property['description']);
+        $address = str_replace("'", "''", $property['address']);
+
+        $query = "UPDATE properties
                       SET
                         property_type_id = {$property['property_type_id']},
                         county = '{$county}',
@@ -53,14 +66,16 @@
                         price = {$property['price']} ,
                         updated_at = '{$property['updated_at']}'
                       WHERE uuid = '{$property['uuid']}'";
-             $statement = $this->conn->prepare($query);
-             $statement->execute();
+        $statement = $this->conn->prepare($query);
+        $result = $statement->execute();
 
-        }
+        return $result;
+    }
 
-        public function insert($property) {
+    public function insert($property)
+    {
 
-            $query = "INSERT INTO property_types (id, title, description, created_at, updated_at)
+        $query = "INSERT INTO property_types (id, title, description, created_at, updated_at)
                       SELECT *
                       FROM (
                         SELECT {$property['property_type']['id']} AS id, '{$property['property_type']['title']}' AS title, '{$property['property_type']['description']}' AS description, '{$property['property_type']['created_at']}' AS created_at, '{$property['property_type']['updated_at']}' AS updated_at
@@ -70,17 +85,17 @@
                         FROM property_types
                         WHERE id = '{$property['property_type']['id']}' AND updated_at >= '{$property['property_type']['updated_at']}' 
                       ) LIMIT 1";
-            $statement = $this->conn->prepare($query);
-            $statement->execute();
+        $statement = $this->conn->prepare($query);
+        $statement->execute();
 
-            //required as some towns had an apostrophe
-            $county = str_replace("'", "''", $property['county'] );
-            $country = str_replace("'", "''", $property['country'] );
-            $town = str_replace("'", "''", $property['town'] );
-            $description = str_replace("'", "''", $property['description'] );
-            $address = str_replace("'", "''", $property['address'] );
+        //required as some towns had an apostrophe
+        $county = str_replace("'", "''", $property['county']);
+        $country = str_replace("'", "''", $property['country']);
+        $town = str_replace("'", "''", $property['town']);
+        $description = str_replace("'", "''", $property['description']);
+        $address = str_replace("'", "''", $property['address']);
 
-            $query = "INSERT INTO properties (uuid, property_type_id, county, country, town, description, address, image_full, image_thumbnail, latitude, longitude, num_bedrooms, num_bathrooms, price, type, created_at, updated_at)
+        $query = "INSERT INTO properties (uuid, property_type_id, county, country, town, description, address, image_full, image_thumbnail, latitude, longitude, num_bedrooms, num_bathrooms, price, type, created_at, updated_at)
                       SELECT *
                       FROM (
                         SELECT
@@ -107,8 +122,9 @@
                         FROM properties
                         WHERE uuid = '{$property['uuid']}' AND updated_at >= '{$property['updated_at']}'
                       ) LIMIT 1";
-            $statement = $this->conn->prepare($query);
-            $statement->execute();
-        }
+        $statement = $this->conn->prepare($query);
+        $result = $statement->execute();
+
+        return $result;
     }
-?>
+}
